b474c22e1af4efab967e36e09c889dbc36191fd2
joe <joe.ulica@gmail.com>
2023-06-27 10:07:35 -0400

    basic CRUD for cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,27 @@
 class CardsController < ApplicationController
+  def index
+    cards = Card.all
+    render json: cards
+  end
+
+  def create
+    card = Card.new(term: params[:term], description: params[:description], familiarity: 1)
+    card.save
+    render json: card
+  end
+
+  def destroy
+    card = Card.find(params[:id])
+    card.destroy
+    render json: {message: "card has been removed"}
+  end
+
+  def update
+    card = Card.find(params[:id])
+    card.term = params[:term] || card.term
+    card.description = params[:description] || card.description
+    card.familiarity = params[:familiarity] || card.familiarity
+    card.save
+    render json: card
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/cards" => "cards#index"
+  post "/cards" => "cards#create"
+  delete "/cards/:id" => "cards#destroy"
+  patch "/cards/:id" => "cards#update"
 end
